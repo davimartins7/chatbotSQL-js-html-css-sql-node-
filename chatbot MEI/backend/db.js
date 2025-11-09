eab51d4e92113540ec20f22a8ad287da87e56def
Davi Martins <unifordavi@gmail.com>
2025-11-08 23:39:20 -0300

    Update database password in db.js

--- chatbot MEI/backend/db.js
+++ chatbot MEI/backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "DaviSQL123@",
+    password: "SENHA",
     database: "login_db"
 });
 
